8a68505eb0ceae582a9f5f1ac37ad5742449be71
GabrielViola95 <92824009+GabrielViola95@users.noreply.github.com>
2022-03-17 13:18:31 -0300

    add task btn and else windtail  styles

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const TaskForm = () => {
   return (
-    <div>
-        TaskForm
+    <div className='flex justify-center items-center h-3/4'>
+        <form className='bg-gray-900 p-10'>
+          <h2 className='mb-7 text-3x1'>A Task</h2>
+          <div className="mb-5">
+            <input type="text" name='title' placeholder='write a title' className='py-3 px-4 focus:tex-gray-100 bg-gray-700 outline-none' />
+          </div>
+          <div className="mb-5">
+            <textarea name="descriptopm" rows="2" placeholder='Write a description' className='py-3 px-4 focus:tex-gray-100 bg-gray-700 outline-none'></textarea>
+          </div>
+          <div>
+            <button className='bg-green-600 w-full hover:bg-green-500 py-2 px-4 mt-5'>
+              Create Task
+            </button>
+            </div>
+        </form>
     </div>
   )
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -8,12 +8,20 @@ const TaskList = () => {
 
   return (
     <div className='flex justify-center'>
+        <div className="w-6/12">
         {tasks.map(task => (
-          <div className='bg-gray-900 px-20 py-5 text-white shadow-2x1 mb-4 flex justify-between'>
+          <div className='bg-gray-900 px-20 py-5 text-white shadow-2x1 mb-4 flex justify-between' key={task.id}>
+            <div>
             <h1>{task.title}</h1>
             <h6>{task.id}</h6>
+            </div>
+            <div>
+              <button>Edit</button>
+              <button>Delete</button>
+            </div>
           </div>
         ))}
+        </div>
     </div>
   )
 }
